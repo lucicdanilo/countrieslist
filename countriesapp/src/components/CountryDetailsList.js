99bb447743a9a782397c03da5e5345920dbdaab7
Danilo Lucic <lucic.danilo94@gmail.com>
2021-01-16 01:07:09 +0100

    adding some styling and functionality to country page

--- countriesapp/src/components/CountryDetailsList.js
+++ countriesapp/src/components/CountryDetailsList.js
@@ -1,6 +1,10 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 function CountryDetailsList(props){
+
+    // Napraviti ove dzave kao zasebne komponente i onda je mnogo lakse sa axiososm....
 
     return (
         <div>
@@ -10,19 +14,19 @@ function CountryDetailsList(props){
             <div><img className="countryFlagImg" src={props.flag} /></div>
             <div>{props.name}</div>
             <div>
-                <div>Native Name: {props.nativeName}</div>
-                <div>Population: {props.population}</div>
-                <div>Region: {props.region}</div>
-                <div>Sub Region: {props.subRegion}</div>
-                <div>Capital: {props.capital}</div>
+                <div><span className="bold">Native Name:</span> {props.nativeName}</div>
+                <div><span className="bold">Population:</span> {props.population}</div>
+                <div><span className="bold">Region:</span> {props.region}</div>
+                <div><span className="bold">Sub Region:</span> {props.subRegion}</div>
+                <div><span className="bold">Capital:</span> {props.capital}</div>
             </div>
             <div>
-                <div>Top Level Domain: {props.topLevelDomain}</div>
-                <div>Currencies: {props.currencies[0].name}</div>
-                <div>Languages: {props.languages[0].name}</div>
+                <div><span className="bold">Top Level Domain:</span> {props.topLevelDomain}</div>
+                <div><span className="bold">Currencies:</span> {props.currencies[0].name}</div>
+                <div><span className="bold">Languages:</span> {props.languages[0].name}</div>
             </div>
-            <div>Border Countries:</div>
-            <div>{props.borderCountries}</div>
+            <div><span></span>Border Countries:</div>
+            <div></div>
         </div>);
 }
 
