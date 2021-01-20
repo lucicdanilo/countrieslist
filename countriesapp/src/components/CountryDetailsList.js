7f6b57b01249129c7f4b18314946cdb65de9b5d0
Danilo Lucic <lucic.danilo94@gmail.com>
2021-01-20 01:45:30 +0100

    adding some responsive styling

--- countriesapp/src/components/CountryDetailsList.js
+++ countriesapp/src/components/CountryDetailsList.js
@@ -14,19 +14,19 @@ function CountryDetailsList(props){
             </Link>
             <div><img className="countryFlagImgDetails" src={props.flag} /></div>
             <div className="countryNameDetails">{props.name}</div>
-            <div className="groupDetails">
+            <div className="groupDetails firstGroup">
                 <div><span className="bold">Native Name:</span> {props.nativeName}</div>
                 <div><span className="bold">Population:</span> {props.population}</div>
                 <div><span className="bold">Region:</span> {props.region}</div>
                 <div><span className="bold">Sub Region:</span> {props.subRegion}</div>
                 <div><span className="bold">Capital:</span> {props.capital}</div>
             </div>
-            <div className="groupDetails">
+            <div className="groupDetails secondGroup">
                 <div><span className="bold">Top Level Domain:</span> {props.topLevelDomain}</div>
                 <div><span className="bold">Currencies:</span> {props.currencies[0].name}</div>
                 <div><span className="bold">Languages:</span> {props.languages[0].name}</div>
             </div>
-            <div className="groupDetails">
+            <div className="groupDetails thirdGroup">
                 <div className="bold">Border Countries:</div>
                 <div className="borderCountries">
                     {props.borderCountries.map(item => 
